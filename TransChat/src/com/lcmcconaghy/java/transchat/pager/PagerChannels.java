7ede20a2c88076f8cc5ef2e0f01a2b6f33ae50e0
lcmcconaghy21 <liam@lcmcconaghy.com>
2020-08-29 21:45:22 -0400

    Fixed interactibility display of the ChannelPager

--- TransChat/src/com/lcmcconaghy/java/transchat/pager/PagerChannels.java
+++ TransChat/src/com/lcmcconaghy/java/transchat/pager/PagerChannels.java
@@ -1,17 +1,26 @@
 package com.lcmcconaghy.java.transchat.pager;
 
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
+import com.lcmcconaghy.java.transchat.cmd.CmdChatFocus;
+import com.lcmcconaghy.java.transchat.cmd.CmdChatListen;
 import com.lcmcconaghy.java.transchat.store.Channel;
 import com.lcmcconaghy.java.transchat.store.ChannelCollection;
 import com.lcmcconaghy.java.transchat.store.ChatUser;
 import com.lcmcconaghy.java.transcore.Message;
+import com.lcmcconaghy.java.transcore.exception.TransCommandException;
 import com.lcmcconaghy.java.transcore.pager.PagerAbstract;
 
 import net.md_5.bungee.api.ChatColor;
 
 public class PagerChannels extends PagerAbstract<Channel>
 {
+	
+	// { FIELDS } //
+	
+	private CmdChatFocus cmdFocus;
+	private CmdChatListen cmdListen;
 	
 	// { SINGLETON } //
 	
@@ -23,6 +32,23 @@ public class PagerChannels extends PagerAbstract<Channel>
 	public PagerChannels()
 	{
 		super("Channel List", ChannelCollection.get());
+		
+		try
+		{
+			this.cmdFocus = new CmdChatFocus();
+		}
+		catch (TransCommandException e)
+		{
+			e.printStackTrace();
+		}
+		try
+		{
+			this.cmdListen = new CmdChatListen();
+		}
+		catch (TransCommandException e)
+		{
+			e.printStackTrace();
+		}
 	}
 	
 	// { PAGER } //
@@ -31,46 +57,26 @@ public class PagerChannels extends PagerAbstract<Channel>
 	public Message sendLine(Channel arg0, CommandSender arg1)
 	{
 		ChatUser user = ChatUser.get(arg1);
-		String preColor = "<e>";
-		boolean canFocus = true;
-		boolean canListen = true;
 		
-		if ( !arg1.hasPermission( arg0.getFocusNode() ) )
-		{
-			preColor = "<c>";
-			canFocus = false;
-			canListen = false;
-		}
+		Message name = new Message(arg0.getDisplayName()+"<f>: ");
+		Message listen = new Message("[L] ").color(ChatColor.RED);
+		Message focus = new Message("[F] ").color(ChatColor.RED);
 		
-		Message name = new Message(preColor+arg0.getDisplayName()+"<f>: ");
-		Message listen = new Message("[L] ");
-		Message focus = new Message("[F] ");
-		
-		if (!user.getFocused().getID().equalsIgnoreCase(arg0.getID()))
+		if (arg1 instanceof Player)
 		{
-			// TODO link focus command
-		}
-		if (!user.isListening(arg0) && canListen)
-		{
-			// TODO link listen command
-		}
-		
-		if (!canListen)
-		{
-			listen.color(ChatColor.RED);
-		}
-		else
-		{
-			listen.color(ChatColor.GREEN);
-		}
-		
-		if (!canFocus)
-		{
-			focus.color(ChatColor.RED);
-		}
-		else
-		{
-			focus.color(ChatColor.GREEN);
+			if (!user.getFocused().getID().equalsIgnoreCase(arg0.getID()) && 
+				user.getPlayer().hasPermission( arg0.getFocusNode() ))
+			{
+				focus = focus.color(ChatColor.GREEN)
+					 .command(this.cmdFocus, arg0.getDisplayName())
+				     .hover("<a>CLICK TO FOCUS THIS CHANNEL");
+			}
+			if (!user.isListening(arg0) && user.getPlayer().hasPermission( arg0.getListenNode() ))
+			{
+				listen = listen.color(ChatColor.GREEN)
+					  .command(this.cmdListen, arg0.getDisplayName())
+				      .hover("CLICK TO LISTEN TO THIS CHANNEL");
+			}
 		}
 		
 		return new Message(name, focus, listen);
