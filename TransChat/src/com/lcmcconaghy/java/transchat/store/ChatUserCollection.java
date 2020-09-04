7effd67aa13f1d95abac9dbf3dc87d887003ee28
lcmcconaghy21 <liam@lcmcconaghy.com>
2020-09-04 07:23:55 +0100

    Added TODO for Channel deletion

--- TransChat/src/com/lcmcconaghy/java/transchat/store/ChatUserCollection.java
+++ TransChat/src/com/lcmcconaghy/java/transchat/store/ChatUserCollection.java
@@ -1,6 +1,12 @@
 package com.lcmcconaghy.java.transchat.store;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.lcmcconaghy.java.transchat.ChatConfig;
+import com.lcmcconaghy.java.transcore.TransPlugin;
 import com.lcmcconaghy.java.transcore.store.UserCollection;
+import com.lcmcconaghy.java.transcore.util.UtilGeneral;
 
 public class ChatUserCollection extends UserCollection<ChatUser>
 {
@@ -21,6 +27,14 @@ public class ChatUserCollection extends UserCollection<ChatUser>
 		super("players", ChatUser.class);
 	}
 	
+	// { INITS } //
 	
+	@Override
+	public void initialize(boolean arg0, TransPlugin arg1)
+	{
+		super.initialize(arg0, arg1);
+		
+		// TODO CATCH WHERE CHANNELS ARE REMOVED WHILE SERVERS ARE OFFLINE
+	}
 	
 }
