e204e3d7156f0edbb3e7fc3799b5d4c6e081d5af
lcmcconaghy21 <liam@lcmcconaghy.com>
2020-09-11 11:56:10 +0100

    Account for obfuscation

--- TransChat/src/com/lcmcconaghy/java/transchat/adapter/AdapterMessage.java
+++ TransChat/src/com/lcmcconaghy/java/transchat/adapter/AdapterMessage.java
@@ -4,6 +4,8 @@ import org.bukkit.entity.Player;
 
 import com.lcmcconaghy.java.transchat.FormatPriority;
 import com.lcmcconaghy.java.transchat.store.Channel;
+import com.lcmcconaghy.java.transcore.Message;
+import com.lcmcconaghy.java.transcore.store.transcore.IUser;
 
 public class AdapterMessage extends AdapterAbstract
 {
@@ -25,6 +27,19 @@ public class AdapterMessage extends AdapterAbstract
 	@Override
 	public String read(Channel arg0, Player arg1, Player arg2, String arg3)
 	{
+		IUser sender = IUser.get(arg1);
+		IUser receiver = IUser.get(arg2);
+		
+		if ( arg0.hasRadius() && arg0.willObfuscate() )
+		{
+			double distance = sender.distanceBetween(receiver);
+			
+			Message temp = new Message(arg3).obfuscate(distance-arg0.getRadiusInner(), 
+					                                   arg0.getRadiusOuter()-arg0.getRadiusInner());
+			
+			return temp.getText();
+		}
+		
 		return arg3;
 	}
 	
